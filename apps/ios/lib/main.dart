121233a5f0ebcafae5d9b27a1243db2b9c3b2b94
wjl-0529 <2913515@qq.com>
2026-07-13 13:38:46 +0800

    fix Flutter lint in native job list

--- apps/ios/lib/main.dart
+++ apps/ios/lib/main.dart
@@ -736,7 +736,7 @@ class _JobsPageState extends State<JobsPage> {
               child: ListView.separated(
                 padding: const EdgeInsets.fromLTRB(16, 8, 16, 24),
                 itemCount: _jobs.length,
-                separatorBuilder: (_, __) => const SizedBox(height: 10),
+                separatorBuilder: (_, _) => const SizedBox(height: 10),
                 itemBuilder: (context, index) {
                   final job = _jobs[index];
                   return JobCard(
